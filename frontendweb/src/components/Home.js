6e39266a3c8cc62516a8c7bd615d402dcd8b4f6a
T5Requiem <147607027+T5Requiem@users.noreply.github.com>
2024-06-04 14:42:36 +0200

    API data display
    API data display

--- frontendweb/src/components/Home.js
+++ frontendweb/src/components/Home.js
@@ -8,7 +8,7 @@ function Home(){
             navigator.geolocation.getCurrentPosition((position) => {
                 const { latitude, longitude } = position.coords;
 
-                fetch(`http://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,daily,alerts&appid=a079118662ea266c81754b94e99980ab`)
+                fetch(`http://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=a079118662ea266c81754b94e99980ab`)
                 .then(response => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
@@ -28,15 +28,65 @@ function Home(){
         }
     }, []);
 
-    let tempCelsius;
-    if (weather && weather.current) {
+    let tempCelsius = 0;
+    let tempCelsiusDay = 0;
+    let tempCelsiusNight = 0;
+    let tempCelsiusMin = 0;
+    let tempCelsiusMax = 0;
+    let sunriseDate, sunriseHours, sunriseMinutes, sunriseStr;
+    let sunsetDate, sunsetHours, sunsetMinutes, sunsetStr;
+    let iconUrlCurrent, iconUrlDaily;
+
+    if (weather && weather.current && weather.daily) {
         tempCelsius = weather.current.temp - 273.15;
+        tempCelsiusDay = weather.daily[0].temp.day - 273.15;
+        tempCelsiusNight = weather.daily[0].temp.night - 273.15;
+        tempCelsiusMin = weather.daily[0].temp.min - 273.15;
+        tempCelsiusMax = weather.daily[0].temp.max - 273.15;
+
+        sunriseDate = new Date(weather.daily[0].sunrise * 1000);
+        sunriseHours = sunriseDate.getHours();
+        sunriseMinutes = "0" + sunriseDate.getMinutes();
+
+        sunsetDate = new Date(weather.daily[0].sunset * 1000);
+        sunsetHours = sunsetDate.getHours();
+        sunsetMinutes = "0" + sunsetDate.getMinutes();
+
+        sunriseStr = sunriseHours + ':' + sunriseMinutes.substr(-2);
+        sunsetStr = sunsetHours + ':' + sunsetMinutes.substr(-2);
+
+        iconUrlCurrent = `http://openweathermap.org/img/wn/${weather.current.weather[0].icon}.png`;
+        iconUrlDaily = `http://openweathermap.org/img/wn/${weather.daily[0].weather[0].icon}.png`;
     }
 
     return (
         <>
-            {weather && weather.current && <p>Current temperature: {tempCelsius.toFixed(2)} °C</p>}
-            {weather && weather.current && weather.current.weather && <p>Weather: {weather.current.weather[0].description}</p>}
+            <h1>Current weather</h1>
+            {weather && weather.current && (
+            <>
+                <img src={iconUrlCurrent} alt="Weather icon" />
+                <p>Description: {weather.current.weather[0].description}</p>
+                <p>Temperature: {tempCelsius.toFixed(2)} °C</p>
+                <p>Pressure: {weather.current.pressure} mb</p>
+                <p>Humidity: {weather.current.humidity} %</p>
+                <p>Wind speed: {weather.current.wind_speed} km/h</p>
+            </>
+             )}
+            <h1>Weather today</h1>
+            {weather && weather.daily && (
+            <>
+                <img src={iconUrlDaily} alt="Weather icon" />
+                <p>Description: {weather.daily[0].weather[0].description}</p>
+                <p>Temperature day: {tempCelsiusDay.toFixed(2)} °C</p>
+                <p>Temperature night: {tempCelsiusNight.toFixed(2)} °C</p>
+                <p>Temperature min: {tempCelsiusMin.toFixed(2)} °C</p>
+                <p>Temperature max: {tempCelsiusMax.toFixed(2)} °C</p>
+                <p>Sunrise: {sunriseStr}</p>
+                <p>Sunset: {sunsetStr}</p>
+                <p>Humidity: {weather.daily[0].humidity} %</p>
+                <p>Wind speed: {weather.daily[0].wind_speed} km/h</p>
+            </>
+             )}
         </>
     );
 }
